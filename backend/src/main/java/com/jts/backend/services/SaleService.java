9689ce9eec23b9eaea31b53d215d1d4c1825edbd
Jeferson Tramonti <jefersontramonti@outlook.com>
2022-07-23 14:32:19 -0300

    API Test

---
+++ backend/src/main/java/com/jts/backend/services/SaleService.java
@@ -0,0 +1,19 @@
+package com.jts.backend.services;
+
+import com.jts.backend.entities.Sale;
+import com.jts.backend.repositories.SaleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SaleService {
+
+    @Autowired
+    private SaleRepository saleRepository;
+
+    public List<Sale> findSales() {
+        return saleRepository.findAll();
+    }
+}
