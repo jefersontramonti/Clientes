7bd63e1cdc89f8ca189217f8c19520784ef9c282
Jeferson Tramonti <jefersontramonti@outlook.com>
2022-07-22 23:51:17 -0300

    Other Components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import Logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={Logo} alt="DSMeta"/>
+                <h1>JST</h1>
+                <p>
+                    Desenvolvido por
+                    <a href="https://www.linkedin.com/in/jeferson-tramonti-38a221182/"> @JefersonTramonti</a>
+                </p>
+            </div>
+        </header>
+    );
+}
+
+export default Header
