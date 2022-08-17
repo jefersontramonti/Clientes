08ba0c5f0aea66942776a5860502650493b44ed7
Jeferson Tramonti <jefersontramonti@outlook.com>
2022-08-16 21:32:32 -0300

    heroku

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -11,7 +11,7 @@ type Props = {
 function handClick(id: number) {
     axios(`${BASE_URL}/sales/${id}/notification`)
         .then(response => {
-            toast.info("SMS enviado com sucesso!!!")
+            toast.info("Enviando SMS")
         })
 }
 
